42bcd0db42365606650eade33e8be6eb280a97a4
Ervin Ebalo <eebalo@gmail.com>
2016-07-04 21:38:38 -0700

    fixed failing test. from empty object to date.now

--- modules/goods/tests/server/good.server.model.tests.js
+++ modules/goods/tests/server/good.server.model.tests.js
@@ -38,7 +38,7 @@ describe('Good Model Unit Tests:', function () {
           firstName: 'Johnny',
           lastName: 'Appleseed',
           email: 'japplesee@gmail.com',
-          receiptDate: {}
+          receiptDate: '2016-01-01'
         },
         categories: ['men', 'women'],
         notes: 'these are notes',
